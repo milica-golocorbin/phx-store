59a755f82c10a98fca71ceefa9b85bd86aa1a096
milicagolocorbin <milicagolocorbin@gmail.com>
2023-03-12 17:51:59 +0100

    clean bolierplate code and fix tests

--- lib/store_web/components/layouts.ex
+++ lib/store_web/components/layouts.ex
@@ -2,4 +2,13 @@ defmodule StoreWeb.Layouts do
   use StoreWeb, :html
 
   embed_templates "layouts/*"
+
+  def get_year(assigns) do
+    {{year, _, _}, _} = :calendar.local_time()
+    assigns = assign(assigns, :year, year)
+
+    ~H"""
+    <h3 class="font-bold"><%= @year %></h3>
+    """
+  end
 end
--- lib/store_web/router.ex
+++ lib/store_web/router.ex
@@ -10,14 +10,14 @@ defmodule StoreWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
+  # pipeline :api do
+  #   plug :accepts, ["json"]
+  # end
 
   scope "/", StoreWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
